71f43c87617ed0f85b78f6d035edd31079d6a23c
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-18 18:53:28 +0100

    Implemented constructor that create panel,textArea and save button

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,11 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -9,6 +14,17 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+
+    public SimpleGUI() {
+        frame.setLayout(new BorderLayout());
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.add(panel, BorderLayout.SOUTH);
+        final JTextArea textArea = new JTextArea();
+        final JButton bSave = new JButton("Save");
+        panel.add(textArea, BorderLayout.CENTER);
+        panel.add(bSave, BorderLayout.CENTER);
+    }
 
     public static void main(String[] args) {
         final SimpleGUI gui = new SimpleGUI();
