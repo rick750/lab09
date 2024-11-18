add27fd861b3721fd59d6cd956943508cfa268c1
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-18 18:42:28 +0100

    Added javadoc

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,7 +1,6 @@
 package it.unibo.mvc;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -17,19 +16,32 @@ public class Controller {
     private static final String DEF_NAME = "output.txt";
     private final File file = new File(DEF_PATH + File.separator + DEF_NAME);
 
+    /**
+     * Provides the current file path
+     * 
+     * @return the current file path
+     */
     public String getPath() {
-        return DEF_PATH;
+        return this.file.getPath();
     }
 
+    /**
+     * Provides the current file
+     * 
+     * @return the current file
+     */
     public File getFile() {
         return this.file;
     }
 
+    /**
+     * Writes {@code str} in the current file
+     * 
+     * @param str
+     */
     public void writeOnFile(final String str) {
         try (PrintStream pStream = new PrintStream(getPath(), StandardCharsets.UTF_8)) {
             pStream.print(str);
-        } catch (FileNotFoundException e1) {
-            logger.log(Level.SEVERE, "File not found", e1);
         } catch (IOException e2) {
             logger.log(Level.SEVERE, "IOException thrown", e2);
         }
