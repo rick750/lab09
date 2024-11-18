390918dd0648eea187d38ef2728419f534e4b36d
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-18 18:31:47 +0100

    Modified and finished writeOnFile(), added logger

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,14 +1,18 @@
 package it.unibo.mvc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private static final Logger logger = Logger.getLogger(System.class.getName());
     private static final String DEF_PATH = System.getProperty("user.home");
     private static final String DEF_NAME = "output.txt";
     private final File file = new File(DEF_PATH + File.separator + DEF_NAME);
@@ -24,8 +28,10 @@ public class Controller {
     public void writeOnFile(final String str) {
         try (PrintStream pStream = new PrintStream(getPath(), StandardCharsets.UTF_8)) {
             pStream.print(str);
-        } catch (IOException e1) {
-            System.out.println(str);
+        } catch (FileNotFoundException e1) {
+            logger.log(Level.SEVERE, "File not found", e1);
+        } catch (IOException e2) {
+            logger.log(Level.SEVERE, "IOException thrown", e2);
         }
     }
 }
