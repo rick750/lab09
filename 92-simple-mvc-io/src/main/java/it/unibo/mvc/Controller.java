8fcec00002cec69ee8acee9918e7aa8996a16c74
Riccardo Carta - riccardo.carta2@studio.unibo.it <riccardo.carta2@studio.unibo.it>
2024-11-18 12:09:02 +0100

    Implemented Controller until writeOnFile method

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,38 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private final File file;
+    private final String PATH = System.getProperty("user.home")
+            + File.separator
+            + Controller.class.getSimpleName() + ".txt";
 
+    public Controller(final File file) {
+        this.file = file;
+    }
+
+    public String getPATH() {
+        return this.PATH;
+    }
+
+    public File getFile() {
+        final File defensiveCopyFile = this.file;
+        return defensiveCopyFile;
+    }
+
+    public boolean writeOnFile(final String str) {
+        try (PrintStream pStream = new PrintStream(PATH, StandardCharsets.UTF_8)) {
+            pStream.print(str);
+        } catch (IOException e1) {
+            System.out.println(str);
+        }
+        return false;
+    }
 }
