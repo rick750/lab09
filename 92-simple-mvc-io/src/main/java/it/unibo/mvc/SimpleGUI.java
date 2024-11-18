2ca6c1ce35be14f42dc5a1ae09142a89ed254784
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-18 19:02:52 +0100

    Implemented display() in order to see the frame with the correct settings

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,8 @@
 package it.unibo.mvc;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,7 +15,7 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
  * 
  */
 public final class SimpleGUI {
-
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame();
 
     public SimpleGUI() {
@@ -28,8 +30,31 @@ public final class SimpleGUI {
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
+    public void display() {
+        /**
+         * Make the frame one fifth the resolution of the screen.
+        */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /** 
+         * The OS window manager take care of the default positioning
+         * on screen.
+         */
+        frame.setLocationByPlatform(true);
+        /**
+         * Resize the frame to the minimum size prior to displaying
+        */
+        frame.pack();
+        /**
+         *  Set the frame as visible 
+         */        
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
         final SimpleGUI gui = new SimpleGUI();
-        //gui.display();
+        gui.display();
     }
 }
