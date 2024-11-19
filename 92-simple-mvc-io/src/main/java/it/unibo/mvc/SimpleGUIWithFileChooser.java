b21de945a767535bcaa71a0250cc7fa87c950fee
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-19 17:42:45 +0100

    Implemented action listeners for the buttons

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -3,13 +3,18 @@ package it.unibo.mvc;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+import javax.swing.undo.StateEditable;
 
 
 /**
@@ -24,7 +29,10 @@ public final class SimpleGUIWithFileChooser {
         // Creating components
         final JPanel mainPanel = new JPanel();
         final JPanel secondPanel = new JPanel();
-        final JTextField textField = new JTextField();
+        final JTextArea textArea = new JTextArea();
+        final JButton btnSave = new JButton("Save");
+        final JTextField textField = new JTextField(controller.getPath());
+        textField.setEditable(false);
         final JButton btnBrowse = new JButton("Browse...");
         // Set layout
         frame.setLayout(new BorderLayout());
@@ -32,8 +40,42 @@ public final class SimpleGUIWithFileChooser {
         secondPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
         frame.add(mainPanel);
         mainPanel.add(secondPanel, BorderLayout.NORTH);
+        mainPanel.add(textArea, BorderLayout.CENTER);
+        mainPanel.add(btnSave, BorderLayout.SOUTH);
         secondPanel.add(textField);
         secondPanel.add(btnBrowse);
+        // Save action
+        btnSave.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(final ActionEvent save) {
+                    // try-catch removed because it generates an error during compilation.
+                    controller.writeOnFile(textArea.getText());
+                }
+            }
+        );
+        // Browse action
+        btnBrowse.addActionListener(
+            new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    final JFileChooser fileChooser = new JFileChooser("Choose the file to save on");
+                    fileChooser.setSelectedFile(controller.getFile());
+                    final int actionSelect = fileChooser.showSaveDialog(frame);
+                    switch (actionSelect) {
+                        case JFileChooser.APPROVE_OPTION:
+                            controller.setFile(fileChooser.getSelectedFile());
+                            textField.setText(controller.getPath());
+                            break;
+                        case JFileChooser.CANCEL_OPTION: 
+                            // Nothing to do
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+                }                
+            };
+        );
         // Frame appearence instructions
         frame.setContentPane(mainPanel);
         final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
