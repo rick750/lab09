333b90ef30c0ffc5deb3c35ccea480c3ad1b97b5
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-19 18:41:02 +0100

    Implemented interface Controller

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,33 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
+
+    /**
+     * 
+     * @param str the next string to print.
+     */
+    void setNextStringToPrint(String str);
+
+    /**
+     * 
+     * @return the next string to print
+     */
+    String getNextStringToPrint();
+
+    /**
+     * 
+     * @return a List rappresenting the history of printed strings
+     */
+    List getPrintedStrings();
+
+    /**
+     * 
+     */
+    void printCurrentString();
 
 }
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,31 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    @Override
+    public void setNextStringToPrint(String str) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List getPrintedStrings() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void printCurrentString() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
