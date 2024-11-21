2cd74aeed8a72835cc13481d03e5b8322e2de323
Riccardo Carta <riccardo.carta2@studio.unibo.it>
2024-11-21 19:15:49 +0100

    Removed unnecessary import for awt event

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -3,7 +3,6 @@ package it.unibo.mvc;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
